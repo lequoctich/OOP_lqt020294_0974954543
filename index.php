8b906bb7173729950d90e0ed23e88c42d3ada3b3
tich <equoctichtichloantichloan@gmail.c>
2019-08-04 17:49:22 +0700

    dcmm

--- index.php
+++ index.php
@@ -1,13 +1,7 @@
 <?php
-echo 'xx';
-echo 'kaka';
-echo 'xx';
+
 echo 'thay doi nhanh cu bi conflict';
-echo 'kaka';echo 'xx';
-echo 'kaka';echo 'xx';
-echo 'kaka';echo 'xx';
-echo 'kaka';echo 'xx';
-echo 'kaka';echo 'xx';
+
 echo 'kaka';
 echo 'kaka';
 echo 'kaka';
